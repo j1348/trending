23b2cef6e0fcffad7ef86eb687268e0ef454cf8c
j1348 <me@jfroffice.me>
2018-04-02 16:27:48 +0200

    /fullrepos return only useful information

--- back/src/index.js
+++ back/src/index.js
@@ -13,41 +13,41 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.json()); // for parsing application/json
 
 app.use((req, res, next) => {
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-	next();
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
 });
 
 // db.repos.find({ 'ticks.date': { $gte: new Date(2018, 2, 20) }  });
 
 app.get('/repos', function(req, res) {
-	const yesterday = moment()
-		.subtract(1, 'day')
-		.toDate();
+    const yesterday = moment()
+        .subtract(1, 'day')
+        .toDate();
 
-	Repo.find(
-		{
-			'ticks.date': {
-				$gte: yesterday,
-			},
-		},
-		'-ticks',
+    Repo.find(
         {
-            sort:{
-                updatedAt: -1,
-                createdAt: -1
-            }
+            'ticks.date': {
+                $gte: yesterday,
+            },
         },
-		(err, repos) => {
-			if (err) {
-				console.error(err);
-				return;
-			}
+        '-ticks',
+        {
+            sort: {
+                updatedAt: -1,
+                createdAt: -1,
+            },
+        },
+        (err, repos) => {
+            if (err) {
+                console.error(err);
+                return;
+            }
 
-			res.setHeader('Cache-Control', 'public, max-age=5000');
-			res.send({ repos });
-		}
-	);
+            res.setHeader('Cache-Control', 'public, max-age=5000');
+            res.send({ repos });
+        }
+    );
 });
 
 app.get('/fullrepos', function(req, res) {
@@ -63,10 +63,10 @@ app.get('/fullrepos', function(req, res) {
         },
         '',
         {
-            sort:{
+            sort: {
                 updatedAt: -1,
-                createdAt: -1
-            }
+                createdAt: -1,
+            },
         },
         (err, repos) => {
             if (err) {
@@ -74,16 +74,34 @@ app.get('/fullrepos', function(req, res) {
                 return;
             }
 
+            const newRepos = repos.map(
+                ({ _id, author, name, href, description, language, createdAt, updatedAt, ticks }) => {
+                    const { stars, forks } = ticks[ticks.length - 1];
+                    return {
+                        _id,
+                        author,
+                        name,
+                        href,
+                        description,
+                        language,
+                        createdAt,
+                        updatedAt,
+                        stars,
+                        forks,
+                    };
+                }
+            );
+
             res.setHeader('Cache-Control', 'public, max-age=5000');
-            res.send({ repos });
+            res.send({ repos: newRepos });
         }
     );
 });
 
 app.get('/importer', function(req, res) {
-	res.send({ running: importer.running });
+    res.send({ running: importer.running });
 });
 
 app.listen(port, function() {
-	console.log(`listening on port ${port}`);
+    console.log(`listening on port ${port}`);
 });
