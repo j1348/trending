fd8d714dd331fce69446058ea95b6687b067bc04
j1348 <me@jfroffice.me>
2018-04-29 16:25:40 +0200

    use endpoint https://trends.abortime.com

--- app/src/App.js
+++ app/src/App.js
@@ -5,16 +5,16 @@ import ApolloClient from 'apollo-boost';
 import { Repos } from './repo';
 
 const client = new ApolloClient({
-  uri: 'http://127.0.0.1:4000/graphql',
+	uri: 'https://trends.abortime.com/graphql',
 });
 
 const App = () => (
-  <ApolloProvider client={client}>
-    <div>
-      <h2>My first Apollo app 🚀</h2>
-      <Repos />
-    </div>
-  </ApolloProvider>
+	<ApolloProvider client={client}>
+		<div>
+			<h2>My first Apollo app 🚀</h2>
+			<Repos />
+		</div>
+	</ApolloProvider>
 );
 
 export default App;
