21c4bad24f240ede463b9042e1fdfc4ee81c5c70
John Fischer <jfroffice@gmail.com>
2018-12-09 16:47:08 +0100

    [map/reduce] Add starByDay

--- src/models/schema.js
+++ src/models/schema.js
@@ -34,4 +34,48 @@ repoSchema.index({ 'ticks.date': 1 });
 
 const Repo = mongoose.model('Repo', repoSchema);
 
+function map() {
+    var lastTicks = this.ticks.slice(Math.max(this.ticks.length - 40, 0));
+    for (var i = 0; i < lastTicks.length; i++) {
+        emit(this.name, lastTicks[i]);
+    }
+}
+
+function reduce(id, ticks) {
+    if (!ticks.length) {
+        return 0;
+    }
+
+    var minStars = ticks[0].stars;
+    var maxStars = minStars;
+    var minDate = ticks[0].date;
+    var maxDate = ticks[ticks.length - 1].date;
+    var tmp = minStars;
+
+    for (var i = 1; i < ticks.length; i++) {
+        tmp = ticks[i].stars;
+        if (tmp > maxStars) {
+            maxStars = tmp;
+        } else if (tmp < minStars) {
+            minStars = tmp;
+        }
+    }
+
+    return Math.round(
+        (24 * 1000 * 3600 * (maxStars - minStars)) / (maxDate - minDate),
+    );
+}
+
+Repo.mapReduce(
+    {
+        map: map.toString(),
+        reduce: reduce.toString(),
+        out: { replace: 'starsByDay' },
+    },
+    (err, results) => {
+        console.log(err);
+        console.log(results);
+    },
+);
+
 module.exports = Repo;
